309af788beab9dc8b8a1d2b8323a6bbdd52b0ef0
kbelous <kbelouswork@gmail.com>
2019-09-25 12:31:09 +0300

    Create py_test.py

---
+++ py_test.py
@@ -0,0 +1,11 @@
+import random
+lucky_num = random.randint(1,10001)
+print "Choose a number"
+
+while True:
+        res = input("Guess a number: ")
+        if res == lucky_num:
+                print "Congrats!"
+                break
+        else:
+                continue
